759f3e68222118042073731f445cb4ce6ecdde86
aquiles <aquiles.martinez@ug.uchile.cl>
2017-07-27 02:56:32 -0400

    test de mapa con respuesta a un pin GPIO

--- test_mazeRato_GPIO.c
+++ test_mazeRato_GPIO.c
@@ -7,23 +7,29 @@ serial version
 ************************/
 
 #include <stdio.h>
-
 #include <stdlib.h>
+#include <time.h>
+
+#include <wiringPi.h>
 
 #define NI 10        /* array sizes */
-
 #define NJ 10
-
 #define NSTEPS 10    /* number of time steps */
 
 int main(int argc, char *argv[]) {
 
 	int i, j, k, ni, nj, pos_v, pos_h;
-	int **map, **new;  
+	int **map;  
 	float x;
+	clock_t start, end;
+
+	if (wiringPiSetupGpio () == -1)
+		exit (1) ;
+
+	pinMode(7, OUTPUT);	
 
   	/* allocate arrays */
-
+	// Inicializacion del mapa
 	  ni = NI + 2;  /* add 2 for left and right ghost cells */
 	  nj = NJ + 2;
 	  map = malloc(ni*sizeof(int*));
@@ -31,7 +37,6 @@ int main(int argc, char *argv[]) {
 
 	  for(i=0; i<ni; i++){
 		    map[i] = malloc(nj*sizeof(int));
-		    //new[i] = malloc(nj*sizeof(int));
 	  }
 
 	  for(i=1; i<=NI; i++){// Posicion vertical
@@ -46,21 +51,25 @@ int main(int argc, char *argv[]) {
 		    }
 	}
 
-	for(i=1; i<=NI; i++){
-		for(j=1; j<=NJ; j++){
-			printf("%d     ", map[i][j]);
-		}
-		printf("\n");
-	}
+	// Desarrollo de la actividad
+	// Imprimir el mapa inicial
+	//for(i=1; i<=NI; i++){
+	//	for(j=1; j<=NJ; j++){
+	//		printf("%d     ", map[i][j]);
+	//	}
+	//	printf("\n");
+	//}
 	
 	pos_v = 1;
 	pos_h = NJ/2;
 
+	start = clock();
 	for(k=1; k<=20; k++){
-		
+		digitalWrite(7, 0);
 		x = rand()/((float)RAND_MAX + 1);
 		if(x<0.25){ //derecha
-			if(pos_v == NI | map[pos_v+1][pos_h] == 1){
+			if(pos_v == NI | map[pos_v+1][pos_h] >= 1){
+			    	digitalWrite(7, 1);
 				continue;			
 			}
 			else{
@@ -69,7 +78,8 @@ int main(int argc, char *argv[]) {
 			}			
 		}
 		else if (x<0.5){//arriba
-			if(pos_h == NJ | map[pos_v][pos_h+1] == 1){
+			if(pos_h == NJ | map[pos_v][pos_h+1] >= 1){
+			    	digitalWrite(7, 1);
 				continue;			
 			}
 			else{
@@ -78,7 +88,8 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		else if (x<0.75){//izquierda
-			if(pos_v == 1 | map[pos_v-1][pos_h] == 1){
+			if(pos_v == 1 | map[pos_v-1][pos_h] >= 1){
+			    	digitalWrite(7, 1);
 				continue;			
 			}
 			else{
@@ -87,7 +98,8 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		else {//abajo
-			if(pos_h == 1 | map[pos_v][pos_h-1] == 1){
+			if(pos_h == 1 | map[pos_v][pos_h-1] >= 1){
+			    	digitalWrite(7, 1);
 				continue;			
 			}
 			else{
@@ -95,17 +107,23 @@ int main(int argc, char *argv[]) {
 				map[pos_v][pos_h] = map[pos_v][pos_h] + 2;
 			}
 		}
-		printf("\n");
 
-		for(i=1; i<=NI; i++){
-		    for(j=1; j<=NJ; j++){
-			 printf("%d     ", map[i][j]);
-			}
-		    printf("\n");
-		}
+		// Imprimir el mapa actualizado
+		//printf("\n");
 
-		map[pos_v][pos_h] = map[pos_v][pos_h] - 2;
+		//for(i=1; i<=NI; i++){
+		//    for(j=1; j<=NJ; j++){
+		//	 printf("%d     ", map[i][j]);
+		//	}
+		//    printf("\n");
+		//}
+		
+		// Borrar el camino previo
+		//map[pos_v][pos_h] = map[pos_v][pos_h] - 2;
 	}
+
+	end = clock();
+	printf("Process took %f seconds to execute \n",  ((double) (end - start)) / CLOCKS_PER_SEC);
 
 	// Evaluar la aleatoriedad -> no aleatorio 
 	//printf("Ten random numbers in [1,100]\n");
@@ -114,7 +132,8 @@ int main(int argc, char *argv[]) {
 	//	k = rand() % 100 + 1;
 	//	printf("%d\n", k);
 	//}
-
+	free(map);	
+	
 	return 0 ;
 }
 
